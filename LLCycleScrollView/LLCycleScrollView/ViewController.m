aaee9eac6bf85578dacee2406f7902cfa820c9b3
liuniuliuniu <416997919@qq.com>
2017-05-08 15:54:46 +0800

    Add .

--- LLCycleScrollView/LLCycleScrollView/ViewController.m
+++ LLCycleScrollView/LLCycleScrollView/ViewController.m
@@ -25,11 +25,10 @@
                         [UIImage imageNamed:@"4"]
                         ];
     
-    
     NSArray *imagesURLStrings = @[
-                                  @"https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a4b3d7085dee3d6d2293d48b252b5910/0e2442a7d933c89524cd5cd4d51373f0830200ea.jpg",
-                                  @"https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a41eb338dd33c895a62bcb3bb72e47c2/5fdf8db1cb134954a2192ccb524e9258d1094a1e.jpg",
-                                  @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg"
+                                  @"http://wx4.sinaimg.cn/mw1024/e67669aagy1ff02eyyt6gj20go0b4wg5.jpg",
+                                  @"http://wx3.sinaimg.cn/mw1024/e67669aagy1ff02f11md6j20go0b6gns.jpg",
+                                  @"http://wx1.sinaimg.cn/mw1024/e67669aagy1ff02f5up20j20go0bcjt3.jpg"
                                   ];
 
     
